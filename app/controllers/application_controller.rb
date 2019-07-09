94585d1cb86a9193a011cfa43fdaf9cf0df87fb6
Muhzi4u <muhsinkeramam@gmail.com>
2019-07-09 19:51:42 +0530

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
+
+  def resource_not_found
+    render json: {
+      errors: [
+        {
+          status: '404',
+          title: "Resource not found",
+          code: '101'
+        }
+      ]
+    }, status: 404
+  end
+    
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+  path: "",
+  path_names: {
+       registration: "signup"
+  },
+  controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations' 
+  },
+  defaults: { format: :json }
 end
